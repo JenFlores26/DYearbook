e4a9dcad941166adcf9484d27b81ad34ba8a06b4
jennifer26 <jenniferflores0001@gmail.com>
2021-07-20 08:05:16 +0800

    removing sample file directing Reg.php to style3

--- Reg.php
+++ Reg.php
@@ -15,7 +15,7 @@
 <head>
   <title>Admin Dashboard</title>
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="styles/sample.css">
+<link rel="stylesheet" href="styles/style3.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <!--<script src="https://code.jquery.com/jquery-3.5.1.min.js" integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>-->
 <link rel="shortcut icon" href="styles/CvSU/logo.ico">
