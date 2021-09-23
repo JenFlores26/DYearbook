5819156fe11a436fbdd156937cc7f199130f6d23
jennifer26 <jenniferflores0001@gmail.com>
2021-09-23 10:00:13 +0800

    filem.php use style6.css

--- Regs.php
+++ Regs.php
@@ -15,7 +15,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <link rel="stylesheet" type="text/css" href="styles/style4.css">
   <link rel="stylesheet" type="text/css" href="styles/style5.css">
-  <link rel="stylesheet" type="text/css" href="styles/style6.css">
+  <link rel="stylesheet" type="text/css" href="styles/style7.css">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 <link rel="shortcut icon" href="styles/CvSU/logo.ico">
 <script src="styles/js/jquery-3.6.0.js"></script>
