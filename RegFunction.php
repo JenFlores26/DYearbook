e5cc259377ae64779b94b346b4729f566dd7fe44
jennifer26 <jenniferflores0001@gmail.com>
2021-11-14 14:02:54 +0800

    edit button for af, ao, grad

--- RegFunction.php
+++ RegFunction.php
@@ -1,114 +1,15 @@
 <?php
-$mysqli = new mysqli('localhost','root','','yearbook') or die(mysqli_error($mysqli));
+include "db_connect.php";
 
-if(isset($_GET['edit'])){
-	$id=$_GET['edit'];
-	$result = $mysqli->query("SELECT * FROM tab2 WHERE id = '$id'") or die($mysqli->error());
+if(isset($_GET['edit1'])){
+	$id=$_GET['edit1'];
+	$result = $db_connect->query("SELECT * FROM tab2 WHERE id = '$id'") or die($mysqli->error());
 	while($row = $result->fetch_assoc()){
 		echo "<html>";
 		echo "<head>";
 		echo "<title>Edit Info</title>";
-		echo "<link rel='stylesheet' type='text/css' href='style2.css'>";
-		echo "<style>";
-		echo "* {
-  margin: 0px;
-  padding: 0px;
-}
-body {
-margin-top:10px;
-  font-size: 120%;
-  background-color:gray;
-  font-family: 'Ubuntu', sans-serif;
-}
-
-.header {
-  width: 30%;
-  margin: 50px auto 0px;
-  color: white;
-  background: #0275d8;
-  text-align: center;
-  border: 1px solid #0275d8;
-  border-bottom: none;
-  border-radius: 10px 10px 0px 0px;
-  padding: 20px;
-}
-form, .content {
-  width: 30%;
-  margin: 0px auto;
-  padding: 20px;
-  border: 1px solid #B0C4DE;
-  background: white;
-  border-radius: 0px 0px 10px 10px;
-}
-.input-group {
-  margin: 10px 0px 10px 0px;
-}
-.input-group label {
-  display: block;
-  text-align: left;
-  margin: 3px;
-}
-.input-group input {
-  height: 30px;
-  width: 93%;
-  padding: 5px 10px;
-  font-size: 16px;
-  border-radius: 5px;
-  border: 1px solid gray;
-  font-family: 'Ubuntu', sans-serif;
-}
-.btn {
-      cursor: pointer;
-        border-radius: 5em;
-        color: #fff;
-        background: linear-gradient(to right, #9C27B0, #E040FB);
-        border: 0;
-        padding-left: 40px;
-        padding-right: 40px;
-        padding-bottom: 10px;
-        padding-top: 10px;
-        font-family: 'Ubuntu', sans-serif;
-        margin-left: 35%;
-        font-size: 13px;
-        box-shadow: 0 0 20px 1px rgba(0, 0, 0, 0.04);
-
-}
-.error {
-  width: 95%;
-  margin: 0px auto;
-  padding: 5px;
-  border: 1px solid #a94442;
-  color: #a94442;
-  background: #f2dede;
-  border-radius: 5px;
-  text-align: left;
-  font-family: 'Ubuntu', sans-serif;
-  font-size: 13px;
-}
-.success {
-  color: #3c763d;
-  background: #dff0d8;
-  border: 1px solid #3c763d;
-  margin-bottom: 20px;
-}
-.button {
-  border-radius: 25px;
-  border:none;
-  color: white;
-  padding: 15px 32px;
-  text-align: center;
-  text-decoration: none;
-  display: inline-block;
-  font-size: 16px;
-  margin: 4px 2px;
-  cursor: pointer;
-
-}
-
-.button1 {background: linear-gradient(to right, red, red);} /* Green */
-.button2 {background: linear-gradient(to right, #9C27B0, #E040FB);} /* Blue */
-}";
-echo "</style>";
+		echo "<link rel='stylesheet' type='text/css' href='styles/style2.css'>";
+		echo "<link rel='stylesheet' type='text/css' href='styles/style7.css'>";
 		echo "</head>";
 		echo "<body>";
 		echo "<div class='header'>
@@ -127,5 +28,61 @@ echo "</style>";
 		echo "</body>";
 		echo "</html>";
 	}
+}
+if(isset($_GET['edit2'])){
+  $id=$_GET['edit2'];
+  $result = $db_connect->query("SELECT * FROM tab3 WHERE id = '$id'") or die($mysqli->error());
+  while($row = $result->fetch_assoc()){
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Edit Info</title>";
+    echo "<link rel='stylesheet' type='text/css' href='styles/style2.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='styles/style7.css'>";
+    echo "</head>";
+    echo "<body>";
+    echo "<div class='header'>
+    <h2>Edit Information</h2>
+  </div>";
+    echo "<form action='s.php' method='POST'>";
+    echo "<div class='card'>";
+        echo "<div class='container'>";
+        echo "<div class='input-group' align='center'>";
+    echo "<input value='" . $row['id'] ."' name='ids' style='display:none;'></input>";
+    echo "<label>First Name:</label><input value='". $row['fname'] ."' name='firstname'></input>";
+    echo "<label>Middle Name:</label><input value='". $row['mname'] ."' name='middlename'></input>";
+    echo "<label>Last Name:</label><input value='". $row['lname'] ."' name='lastname'></input>";
+    echo "<button type ='submit' class='button button1' name='save'>SAVE</a></button>";
+    echo "</form>";
+    echo "</body>";
+    echo "</html>";
+  }
+}
+if(isset($_GET['edit3'])){
+  $id=$_GET['edit3'];
+  $result = $db_connect->query("SELECT * FROM shs WHERE id = '$id'") or die($mysqli->error());
+  while($row = $result->fetch_assoc()){
+    echo "<html>";
+    echo "<head>";
+    echo "<title>Edit Info</title>";
+    echo "<link rel='stylesheet' type='text/css' href='styles/style2.css'>";
+    echo "<link rel='stylesheet' type='text/css' href='styles/style7.css'>";
+    echo "</head>";
+    echo "<body>";
+    echo "<div class='header'>
+    <h2>Edit Information</h2>
+  </div>";
+    echo "<form action='s.php' method='POST'>";
+    echo "<div class='card'>";
+        echo "<div class='container'>";
+        echo "<div class='input-group' align='center'>";
+    echo "<input value='" . $row['id'] ."' name='ids' style='display:none;'></input>";
+    echo "<label>First Name:</label><input value='". $row['fname'] ."' name='firstname'></input>";
+    echo "<label>Middle Name:</label><input value='". $row['mname'] ."' name='middlename'></input>";
+    echo "<label>Last Name:</label><input value='". $row['lname'] ."' name='lastname'></input>";
+    echo "<button type ='submit' class='button button1' name='save'>SAVE</a></button>";
+    echo "</form>";
+    echo "</body>";
+    echo "</html>";
+  }
 }
 ?>
